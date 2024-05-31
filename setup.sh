affd75e709e702c73cf03292110d5c596fecbbca
Christopher Penkin <chris@penkin.me>
2024-05-31 10:47:56 +0200

    Added setup and initial test file.

---
+++ setup.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# make sure we have pulled in and updated any submodules
+# git submodule init
+# git submodule update
+
+# what directories should be installable by all users including the root user
+base=(
+)
+
+# folders that should, or only need to be installed for a local user
+useronly=(
+  .zsh
+)
+
+# run the stow command for the passed in directory ($2) in location $1
+stowit() {
+    usr=$1
+    app=$2
+    # -v verbose
+    # -R recursive
+    # -t target
+    stow -v -R -t ${usr} ${app}
+}
+
+echo ""
+echo "Stowing apps for user: ${whoami}"
+
+# install apps available to local users and root
+for app in ${base[@]}; do
+    stowit "${HOME}" $app 
+done
+
+# install only user space folders
+if [ "$EUID" -ne 0 ]; then
+  for app in ${useronly[@]}; do
+    stowit "${HOME}/$app" $app 
+  done
+fi
+
+echo ""
+echo "##### ALL DONE"
