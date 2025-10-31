fb4c841ff923909093e2978d117ca8b4ea097346
Christopher Penkin <chris@penkin.me>
2025-10-31 14:44:44 +0200

    Updated nvim snacks setup for zen mode.

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -121,5 +121,30 @@ return {
             end,
             desc = "Select Scratch Buffer",
         },
+
+        -- ZEN MODE
+        {
+            "<leader>fz",
+            mode = { "n" },
+            function()
+                Snacks.zen({
+                    enter = true,
+                    fixbuf = false,
+                    minimal = false,
+                    width = 120,
+                    height = 0,
+                    backdrop = { transparent = false, blend = 40 },
+                    keys = { q = false },
+                    zindex = 40,
+                    wo = {
+                        winhighlight = "NormalFloat:Normal",
+                    },
+                    w = {
+                        snacks_main = true,
+                    },
+                })
+            end,
+            desc = "Scratch Buffer",
+        },
     },
 }
